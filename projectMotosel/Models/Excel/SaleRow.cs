ba85ce4624b0aa47da02d6067561cbfc894d5f3f
Huck5 <fcharleslundaahl@gmail.com>
2015-05-06 19:45:44 -0700

    Prep for Demo. Removed unused/confusing models. Reseed migrations

---
+++ projectMotosel/Models/Excel/SaleRow.cs
@@ -0,0 +1,30 @@
+﻿using MotoselProject.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace projectMotosel.Models.Excel
+{
+    public class SaleRow
+    {
+        [Key]
+        public int SaleRowId { get; set; }
+        [Required]
+        public int SaleId { get; set; }
+        [Required]
+        public string SKU { get; set; }
+
+        [ForeignKey("SaleId")]
+        public virtual Sale Sale { get; set; }
+        [ForeignKey("SKU")]
+        public virtual Product Product { get; set; }
+
+        [Required] 
+        public int Quantity { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
